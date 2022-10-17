d11f26f0135e20151089b3bee00113e630ad5409
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2022-10-17 23:10:48 +0530

    Time: 58 ms (22.45%), Space: 23.2 MB (75.32%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -5,7 +5,7 @@ public:
         int len = s.size();
         int i = 0, j = len-1;
         
-        while(i <= j)
+        while(i < j)
         {
             char ch = s[i];
             s[i] = s[j];
