6840bd18e4af5449e5ddbe6c86bc4bfe0cec19b7
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2022-10-17 23:08:51 +0530

    Time: 50 ms (40.06%), Space: 23.3 MB (40.14%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) 
+    {
+        int len = s.size();
+        int i = 0, j = len-1;
+        
+        while(i <= j)
+        {
+            char ch = s[i];
+            s[i] = s[j];
+            s[j] = ch;
+            
+            i++;
+            j--;
+        }
+    }
+};
