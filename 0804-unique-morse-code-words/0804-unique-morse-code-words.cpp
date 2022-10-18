1e3b138ac73cec33849495f6937d0adc2c6cbe01
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2022-10-18 23:13:12 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (32.29%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
@@ -6,13 +6,28 @@ public:
         
         unordered_set<string> st;
         
-        for (auto w : words) 
+        
+        // for (auto w : words) 
+        // {
+        //     string str;
+        //     for (auto c : w)
+        //     {
+        //         str += vec[c - 'a'];
+        //     }
+        //     st.insert(str);
+        // }
+        int len1 = words.size();
+        
+        for(int i = 0; i < len1; i++)
         {
-            string str;
-            for (auto c : w)
+            string str = "";
+            int len2 = words[i].size();
+            
+            for(int j = 0; j < len2; j++)
             {
-                str += vec[c - 'a'];
+                str += vec[words[i][j] - 'a'];
             }
+            
             st.insert(str);
         }
         
