57e554e736d6bd9041d55abf342228a55a94fd82
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2022-10-18 23:15:06 +0530

    Time: 7 ms (53.14%), Space: 8.7 MB (32.29%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
@@ -5,17 +5,6 @@ public:
         vector<string> vec = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
         
         unordered_set<string> st;
-        
-        
-        // for (auto w : words) 
-        // {
-        //     string str;
-        //     for (auto c : w)
-        //     {
-        //         str += vec[c - 'a'];
-        //     }
-        //     st.insert(str);
-        // }
         int len1 = words.size();
         
         for(int i = 0; i < len1; i++)
