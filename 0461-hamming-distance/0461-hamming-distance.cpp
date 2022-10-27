7f58fde858a00c1cdc09fe21eb134b4385a15051
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2022-10-27 16:53:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.63%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) 
+    {
+        int res = 0;
+        int num = x^y;
+        
+        while (num != 0) 
+        {
+            res += num % 2;
+            num >>= 1;
+        }
+        return res;
+    }
+};
