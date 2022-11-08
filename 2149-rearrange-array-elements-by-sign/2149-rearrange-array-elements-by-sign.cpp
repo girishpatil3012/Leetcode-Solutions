236016cdedbd1dba0e704d02cf5c06687291c0cb
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2022-11-08 12:42:09 +0530

    Time: 494 ms (49.39%), Space: 124 MB (64.77%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) 
+    {
+        int len = nums.size();
+        int index1 = 0, index2 = 1;
+        vector<int> vec(len,0);
+        
+        for(int i = 0; i < len; i++)
+        {
+            if(nums[i] > 0)
+            {
+                vec[index1] = nums[i];
+                index1 += 2;
+            }
+            
+            if(nums[i] < 0)
+            {
+                vec[index2] = nums[i];
+                index2 += 2;
+            }
+        }
+        return vec;
+    }
+};
