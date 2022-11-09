f495bec5943b322bf5290d47d293ce46f5c5d464
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2022-11-09 12:36:58 +0530

    Time: 99 ms, Memory: 22.5 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) 
+    {
+        int hill = 0, valley = 0;
+        int size = arr.size();
+        
+        for (int i = 1; i < size - 1; i++) 
+        {
+            if (arr[i - 1] == arr[i] or arr[i] == arr[i + 1])
+            {
+                return false;
+            }  
+            if (arr[i - 1] < arr[i] and arr[i] > arr[i + 1])
+            {
+                hill++;
+            }
+            if (arr[i - 1] > arr[i] and arr[i] < arr[i + 1])
+            {
+                valley++;
+            }      
+        }
+        return hill == 1 and valley == 0;
+    }
+};
