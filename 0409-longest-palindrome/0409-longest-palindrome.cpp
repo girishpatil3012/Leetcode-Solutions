d926f283b99c13ff5615d71e32fda10b47d23038
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2022-12-30 19:35:30 +0530

    Time: 4 ms (67.13%), Space: 6.7 MB (16.29%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestPalindrome(string s) 
+    {
+        unordered_map<int,int> mp;
+        int res = 0;
+        int len = s.length();
+        
+        for(int i = 0; i < len; i++) 
+        {
+            mp[s[i]]++;
+            if(mp[s[i]] % 2 == 0)
+            {
+                res += mp[s[i]];
+                mp[s[i]] = 0;
+            }
+        }
+        
+        for(auto x: mp)
+        {
+            if(x.second == 1) 
+            {
+              res++;
+              break;
+            }  
+        }
+        return res;
+    }
+};
