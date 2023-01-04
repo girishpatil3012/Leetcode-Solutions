43849fa5a60817c8e22b0d35b8be463239541e14
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2023-01-04 23:00:34 +0530

    Time: 33 ms (15.38%), Space: 5.9 MB (86.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,27 +2,11 @@ class Solution {
 public:
     int fib(int n) 
     {
-        if(n < 2)
-        {
-            return n;
-        }
-        int a = 0, b = 1, c = 0;
+        if(n ==0)
+            return 0;
+        if(n == 1)
+            return 1;
         
-        for(int i = 1; i < n; i++)
-        {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        
-        return c;
+        return fib(n-1) + fib(n-2);
     }
 };
-/*
-int fib(int N) 
-{
-    if(N == 0)  return 0;
-    if(N == 1)  return 1;
-    return fib(N-1) + fib(N-2);
-}
-*/
