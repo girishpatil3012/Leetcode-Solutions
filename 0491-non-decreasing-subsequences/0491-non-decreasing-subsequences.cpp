6c3ec80d446415b9982a210eb7a05fcf5c41a7a8
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2023-01-20 09:46:09 +0530

    Time: 35 ms (96.55%), Space: 16.2 MB (99.34%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solving(vector<int>& nums, vector<vector<int>>& output, vector<int>& temp, int index){
+        if(index >= nums.size()){
+            if(temp.size() > 1)
+                output.push_back(temp);
+            return;
+        }
+        if(index==0 || temp.size()==0 || nums[index] >= temp[temp.size()-1]){
+            temp.push_back(nums[index]);
+            solving(nums, output, temp, index+1);
+            temp.pop_back();
+        }
+        if(temp.size()==0 || temp[temp.size()-1]!=nums[index]){
+            solving(nums, output, temp, index+1);
+        }
+}
+	
+    vector<vector<int>> findSubsequences(vector<int>& nums) 
+    {
+        vector<vector<int>> output;
+        vector<int> temp;
+        solving(nums, output, temp, 0);
+        return output;
+    }
+};
