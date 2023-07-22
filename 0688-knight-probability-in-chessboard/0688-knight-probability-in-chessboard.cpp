290d51d82909ca8bd785f5d1769e9d81b42af85e
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2023-07-22 11:21:39 +0530

    Time: 5 ms (95.48%), Space: 6.8 MB (77.96%) - LeetHub

---
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dirs[8][2] = {{2,-1}, {2, 1}, {-2,-1}, {-2, 1}, {1, -2}, {1, 2}, {-1, -2}, {-1, 2}}; 
+    double dp[30][30][105];
+
+    double find(int row, int col, int n, int move) {
+        if(row < 0 || row >= n || col < 0 || col >= n) return 0;
+        if(move == 0) return 1;
+
+        if(dp[row][col][move]) return dp[row][col][move];
+
+        double prob = 0.0;
+        for(int i=0; i<8; i++) {
+            prob += find(row + dirs[i][0], col + dirs[i][1], n, move - 1) / 8.0;
+        }
+        return dp[row][col][move] = prob;
+    }
+    double knightProbability(int n, int k, int row, int col) {
+        memset(dp, 0, sizeof(dp));
+        return find(row, col, n, k);
+    }
+};
