95bd652dde82f85f0072dc3a7a4d1c7236abcbfc
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2023-07-24 13:59:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (62.65%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double myPow(double x, int n) 
+    {
+        if(n==0) 
+            return 1;
+        double temp = myPow(x,n/2);
+        if(n%2 == 0) 
+            return temp*temp;
+        return n<0 ? (1/x)*temp*temp:x*temp*temp;
+    }
+};
