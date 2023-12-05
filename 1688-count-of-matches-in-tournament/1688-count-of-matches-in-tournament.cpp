9a6646378c1f647ec43caef09dbc3dc17133086b
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2023-12-05 22:01:19 +0530

    Time: 3 ms (17.67%), Space: 6.4 MB (13.68%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
-    int numberOfMatches(int n) 
-    {
-        return n-1;
+    // time/space: O(logn)/O(1)
+    int numberOfMatches(int n) {
+        int result = 0;
+        while (n >= 2) {
+            int matches = (n / 2);
+            int losers = matches;   // `k` matches produce `k` losers
+            result += matches;      // count the matches
+            n -= losers;            // remove the losers
+        }
+        return result;
     }
 };
