656a443911e4302d3a1be47196b2338212f1148f
Girish Patil <99480229+girishpatil3012@users.noreply.github.com>
2024-03-28 10:37:46 +0530

    Time: 174 ms (97.27%), Space: 148.5 MB (57.21%) - LeetHub

---
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -0,0 +1,27 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n=nums.size(), cnt=0;
+        unordered_map<int, int> freq;//1 <= nums[i] <= 10^9
+        for (int l=0, r=0; r<n; r++){
+            int x=nums[r];
+            if (freq.count(x)==0) 
+                freq[x]=1;
+            else freq[x]++;
+            while (freq[x]>k){
+                freq[nums[l++]]--;
+            }
+            cnt=max(cnt,r-l+1);// max len
+        }
+        return cnt;
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
